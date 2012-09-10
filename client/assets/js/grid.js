6a9371ca78face151da43feaa2b10bee970e7aed
Simon Elliott <simon@papercreatures.com>
2012-09-10 20:49:23 +0100

    fix

--- client/assets/js/grid.js
+++ client/assets/js/grid.js
@@ -76,7 +76,7 @@ $(document).ready(function () {
       sandbox.iframe[0].contentWindow.Broadcast = window.Broadcast;
       injectCSS(sandbox.iframe, "/css/screen.css", function () {
         injectScript(sandbox.iframe, "/socket.io/socket.io.js", function () {
-          injectScript(sandbox.iframe, "/js/broadcast.js", function () {
+          injectScript(sandbox.iframe, "/js/vendor/broadcast.js", function () {
             injectScript(sandbox.iframe, "/js/sandbox.js", function () {
               sandbox.iframe[0].contentWindow.createCanvas(sandbox.id);
               injectScript(sandbox.iframe, script.src + "?" + Math.random(), function () {
