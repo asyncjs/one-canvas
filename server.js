d3b6cb50deff4c0f82aa037dc47c75cde5e1e0f8
Simon Elliott <simon@papercreatures.com>
2012-09-10 19:21:29 +0100

    list endpoint

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 var express = require('express')
   , app = express()
+  , fs = require('fs')
+  , path = require('path')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
   , argv = require('optimist')
@@ -17,12 +19,29 @@ app.get('/client/:id', function (req, res) {
   res.sendfile(__dirname + '/client/index.html');
 });
 
-app.get('canvas/:id/view', function(req, res) {
+app.get('/canvas/:id/view', function(req, res) {
   
 });
 
-app.get('canvas/:id/edit', function(req, res) {
+app.get('/canvas/:id/edit', function(req, res) {
   
+});
+
+app.get('/list', function(req, res) {
+  var base = "src/";
+  var resp = [];
+  fs.readdir(base, function(err, files) {
+    files.forEach(function(file) {
+      if(/\.(js|coffee)$/.test(file)) {
+        var stat = fs.statSync(base+file);
+        resp.push({
+          ts: Date.parse(stat.mtime),
+          src: "/get/" + file
+        });
+      }
+    });
+    res.send(200, resp);
+  });
 });
 
 io.sockets.on('connection', function (socket) {
