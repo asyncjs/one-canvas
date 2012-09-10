a6f370ca0b830bda853fda62f2d0dfb4bbc6d593
Aron Carroll <self@aroncarroll.com>
2012-09-10 20:17:31 +0100

    Remove ready function as its fucked

--- canvases/example.js
+++ canvases/example.js
@@ -1,12 +1,13 @@
-sandbox.ready(function () {
+//canvas.ready(function () {
   var canvas = document.createElement('canvas');
   canvas.width = document.width;
   canvas.height = document.height;
 
   var ctx = canvas.getContext('2d');
   ctx.beginPath();
+  ctx.fillStyle = 'red'
   ctx.arc(canvas.width, canvas.height, canvas.height / 2, 0, Math.PI, true);
   ctx.fill();
 
   document.body.appendChild(canvas);
-});
+//});
