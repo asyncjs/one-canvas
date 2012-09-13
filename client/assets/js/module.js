b610f6d86a7e35adb246d255696dba6143761a9f
Simon Elliott <simon@papercreatures.com>
2012-09-13 21:21:40 +0100

    update websocket

--- client/assets/js/module.js
+++ client/assets/js/module.js
@@ -102,7 +102,7 @@
 
   window.createModule = function (id) {
     if (window.module) { return window.module; }
-    var socket = io.connect();
+    var socket = io.connect("http://canvas.browsing.co.uk");
     var module = window.module = new Sandbox();
     var client = new Client(id, module, socket);
 
