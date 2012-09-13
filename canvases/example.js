09625f044761c6bddfb329dac616c0ef4ab31b9f
Thomas Parslow <tom@almostobsolete.net>
2012-09-13 18:22:07 +0100

    Improved example :)

--- canvases/example.js
+++ canvases/example.js
@@ -2,7 +2,9 @@ var jQuery = require('jquery');
 
 var ctx = canvas.getContext('2d');
 ctx.beginPath();
-ctx.fillStyle = 'red'
+
+
+ctx.fillStyle = '#' + Math.floor(Math.random()*16).toString(16) + Math.floor(Math.random()*16).toString(16) + Math.floor(Math.random()*16).toString(16);
 ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 4, 0, Math.PI * 2, true);
 ctx.fill();
 
--- canvases/example2.js
+++
@@ -1,9 +0,0 @@
-var jQuery = require('jquery');
-
-var ctx = canvas.getContext('2d');
-ctx.beginPath();
-ctx.fillStyle = 'blue'
-ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 4, 0, Math.PI * 2, true);
-ctx.fill();
-
-document.body.appendChild(canvas);
