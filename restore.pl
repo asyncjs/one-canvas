4f675cadd12b315d725e7c0def91192d96561226
Simon Elliott <simon@papercreatures.com>
2012-09-27 19:31:18 +0100

    restore

--- restore.pl
+++ restore.pl
@@ -5,6 +5,8 @@ use IO::All;
 use LWP::UserAgent;
 my $base = "http://onecanvas.asyncjs.com";
 my $ua = LWP::UserAgent->new;
+foreach(@{decode_json(io('backup.txt')->all)}) {
+  my $id = (split("[/\.]", $_->{'src'}))[2];
+  $ua->request(POST "$base/canvas/$id/save", [publish => 1, content => $_->{content}] );
+}
 
-sub get { $ua->request(GET "$base$_[0]")->content;}
-encode_json [map { $_->{content} = get($_->{src}."?clean=1");$_ } @{decode_json(get("/list"))}] > io 'backup.txt'
